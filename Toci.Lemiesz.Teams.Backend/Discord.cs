1e19f91d6d7ea5dbf4e106b2d2a814ef1fd7f320
toci888 <bzapart@gmail.com>
2024-11-22 05:39:06 +0100

    xd.

---
+++ Toci.Lemiesz.Teams.Backend/Discord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+class ProgramDiscord
+{
+    private DiscordSocketClient _client;
+
+    static void Main(string[] args) => new ProgramDiscord().RunBotAsync().GetAwaiter().GetResult();
+
+    public async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        // Token bota
+        string botToken = "YOUR_DISCORD_BOT_TOKEN";
+
+        _client.Log += Log;
+
+        await _client.LoginAsync(TokenType.Bot, botToken);
+        await _client.StartAsync();
+
+        _client.MessageReceived += HandleMessageReceived;
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage log)
+    {
+        Console.WriteLine(log.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task HandleMessageReceived(SocketMessage message)
+    {
+        if (message.Author.IsBot) return;
+
+        if (message.Content.StartsWith("!hello"))
+        {
+            await message.Channel.SendMessageAsync($"Witaj, {message.Author.Username}! Jak mogę Ci pomóc?");
+        }
+    }
+}
---
+++ Toci.Lemiesz.Teams.Backend/Teams.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Konfiguracja aplikacji
+        string clientId = "YOUR_CLIENT_ID";
+        string tenantId = "YOUR_TENANT_ID";
+        string clientSecret = "YOUR_CLIENT_SECRET";
+
+        // Uwierzytelnianie przy użyciu MSAL
+        var confidentialClient = ConfidentialClientApplicationBuilder
+            .Create(clientId)
+            .WithTenantId(tenantId)
+            .WithClientSecret(clientSecret)
+            .Build();
+
+        string[] scopes = { "https://graph.microsoft.com/.default" };
+        var authResult = await confidentialClient.AcquireTokenForClient(scopes).ExecuteAsync();
+
+        // Inicjalizacja Graph Client
+        var graphClient = new GraphServiceClient(new DelegateAuthenticationProvider(requestMessage =>
+        {
+            requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            return Task.CompletedTask;
+        }));
+
+        // Pobranie użytkowników z Microsoft Teams
+        var users = await graphClient.Users.Request().GetAsync();
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"User: {user.DisplayName}, Email: {user.Mail}");
+        }
+    }
+}
